13f3907a2b245c689733ac545cde28d03e4acd08
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-05-26 18:32:18 -0700

    Methods Revisited answered

--- lab-answer.js
+++ lab-answer.js
@@ -77,10 +77,66 @@
 // * Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo).
 // * Use a for of loop to call toUpperCase() on each of them and print out the result.
 
-const ninja = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+// const ninja = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+// let index = 0;
+// for(let i of ninja) {
+//   ninja[index] = i.toUpperCase()
+//   index ++
+// }
+// console.log(ninja)
+
+//Methods, Revisited
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+// * Console log the index of Titanic.
+
+console.log(favMovies.indexOf('Titanic'))
+
+// // sort()
+
+console.log(favMovies.sort()) // Its sort the array in assending order
+
+// // pop()
+
+console.log(favMovies.pop())  // It removes the last element from the Array
+
+// //push()
+
+favMovies.push('Guardians of the Galaxy') // It adds new element at end of the array
+
+// // reverse()
+favMovies.reverse() // It reverse the array 
+
+// //shift()
+console.log(favMovies.shift()) // It removes the last element from the array.
+
+// //unshift()
+console.log(favMovies.unshift('You')) // It add new element at bignning of the array
+console.log(favMovies)
+
+//splice() - Django Unchained" and add "Avatar"
+
 let index = 0;
-for(let i of ninja) {
-  ninja[index] = i.toUpperCase()
-  index ++
+for(let i of favMovies) {
+  if(i ==='Django Unchained') {
+    favMovies.splice(index, 1, 'Avatar') // using splice() we can remove a specific element and add element on specific index
+  }
+  if(index > favMovies.length){
+    break
+  } 
+  
+index ++
 }
-console.log(ninja)
+
+console.log(favMovies)
+
+//slice()
+//slice the last half of the array. (Challenge yourself and try to programatically determine the middle of the array rather than counting it and hard coding it.)
+
+let half = (favMovies.length -1) / 2;
+console.log(favMovies.slice(half)) // we sliced the array in half permanently. 
+
+console.log(favMovies.indexOf('Fast and Furious'))
+
+// the index of the 'Fast and Furious' is still 18. hmm ...
